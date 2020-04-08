345fb5d693189554c4a59d20459946345e416f65
李良奇 <codedance98@gmail.com>
2020-04-08 19:43:15 +0800

    rm sass-loader node-sass

--- examples/App.vue
+++ examples/App.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style lang='scss'>
+<style>
 *{
   padding: 0;
   margin: 0;
--- packages/ohmybox/src/ohmybox.vue
+++ packages/ohmybox/src/ohmybox.vue
@@ -143,20 +143,20 @@ export default {
 	}
 }
 </script>
-<style lang="scss">
+<style scoped>
 #ohmybox-component {
 	position: relative;
 	overflow: hidden;
 	width:100vw;
 	height:100vh;
-	.ohmybox-gesture {
-		-webkit-transform: translate3d(0, 0, 0);
-		-webkit-backface-visibility: hidden;
-		-webkit-perspective: 1000;
-		will-change: --webkit-transform;
-	}
-	.trans {
-		-webkit-transition: all 0.3s ease-out;
-	}
+}
+.ohmybox-gesture {
+	-webkit-transform: translate3d(0, 0, 0);
+	-webkit-backface-visibility: hidden;
+	-webkit-perspective: 1000;
+	will-change: --webkit-transform;
+}
+.trans {
+	-webkit-transition: all 0.3s ease-out;
 }
 </style>
