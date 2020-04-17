15c5c0ef0708b82b04598f48a85f32d589dd1b34
李良奇 <codedance98@gmail.com>
2020-04-17 14:47:10 +0800

    增加click方法 模拟点击两个参数 增加window.onresize方法重置item高度

--- examples/App.vue
+++ examples/App.vue
@@ -4,9 +4,12 @@
       :scrollHeight='scrollHeight'
       :quickScrollHeight='quickScrollHeight'
       :scrollDuration='scrollDuration'
+      :clickDuration='clickDuration'
+      :clickDistance='clickDistance'
       @change='change'
+      @click='click'
     >
-      <OhMyBoxItem v-for='(item, idx) in 5' :key='idx'>{{item}}</OhMyBoxItem>
+      <OhMyBoxItem class='item' v-for='(item, idx) in 5' :key='idx'>{{item}}</OhMyBoxItem>
     </OhMyBox>
   </div>
 </template>
@@ -19,11 +22,16 @@ export default {
     return {
       scrollHeight:300,
       quickScrollHeight:20,
-      scrollDuration: 400
+      scrollDuration: 400,
+      clickDuration:10,
+      clickDistance:30
     }
   },
   methods:{
     change(idx, total, direction){
+      console.log(idx, total, direction)
+    },
+    click(idx, total, direction){
       console.log(idx, total, direction)
     }
   },
@@ -38,5 +46,13 @@ export default {
 *{
   padding: 0;
   margin: 0;
+}
+.item{
+  width:100vw;
+  height:100vh;
+  background:gray;
+  text-align:center;
+  font-size: 50px;
+  border-bottom:1px solid red;
 }
 </style>
